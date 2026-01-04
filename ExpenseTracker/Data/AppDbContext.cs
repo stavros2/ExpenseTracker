5253cf8de6e0c4ea139224e8ca347d7e3cdcbd9f
Stavros Stavrou <stavros_stavrou1@hotmail.com>
2026-01-04 15:03:41 +0200

    Simple Implementation of AppDbContext (: DbContext)

---
+++ ExpenseTracker/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
--- ExpenseTracker/Models/DTOs/ExpenseDTO.cs
+++ ExpenseTracker/Models/DTOs/ExpenseDTO.cs
@@ -33,4 +33,3 @@ namespace ExpenseTracker.Models.DTOs
 
     }
 }
-}
