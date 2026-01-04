f8e6e573341b69bb0454b8476551c694e56d2010
Stavros Stavrou <stavros_stavrou1@hotmail.com>
2026-01-04 15:04:19 +0200

    Initial Interface and Class for interacting with the DB

---
+++ ExpenseTracker/Repositories/ExpenseRepository.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Repositories
+{
+    public class ExpenseRepository : IExpenseRepository
+    {
+        public async Task AddExpenseAsync(Models.Expense expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteExpenseAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Expense>> GetAllExpensesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Expense?> GetExpenseByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateExpenseAsync(int id, Expense expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        pu
+    }
+}
---
+++ ExpenseTracker/Repositories/IExpenseRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Repositories
+{
+    public interface IExpenseRepository
+    {
+        Task<IEnumerable<Expense>> GetAllExpensesAsync();
+        Task<Expense?> GetExpenseByIdAsync(int id);
+        Task AddExpenseAsync(Expense expense);
+        Task UpdateExpenseAsync(int id, Expense expense);
+        Task DeleteExpenseAsync(int id);
+    }
+}
