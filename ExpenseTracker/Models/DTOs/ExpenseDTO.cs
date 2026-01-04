934c9f911aba2d481c4e4b7a645941dbce8aea0c
Stavros Stavrou <stavros_stavrou1@hotmail.com>
2026-01-04 12:51:01 +0200

    Define Data models for DB and RestAPI endpoints

---
+++ ExpenseTracker/Models/DTOs/ExpenseDTO.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ExpenseTracker.Models.DTOs
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ExpenseCategory
+    {
+        Unknown = 0,
+        Food,
+        Transportation,
+        Utilities,
+        Entertainment,
+        Healthcare,
+        Other
+    }
+
+
+    public class ExpenseDTO
+    {
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime ExpenseDate { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero.")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [Range(1, 6, ErrorMessage = "Invalid category.")]
+        public ExpenseCategory Category { get; set; } = ExpenseCategory.Unknown;
+
+    }
+}
+}
---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public string Description { get; set; } = string.Empty;
+        public DateTime ExpenseDate { get; set; }
+        public double Amount { get; set; }
+        public string Category { get; set; } = string.Empty;
+
+    }
+}
